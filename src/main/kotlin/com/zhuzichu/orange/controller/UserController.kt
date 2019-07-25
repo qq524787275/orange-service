a1031ea0c584556f292b213f21398c4de2cc5739
524787275@qq.com <qaioasd520>
2019-07-25 08:21:38 +0800

    update

--- src/main/kotlin/com/zhuzichu/orange/controller/UserController.kt
+++ src/main/kotlin/com/zhuzichu/orange/controller/UserController.kt
@@ -1,6 +1,10 @@
 package com.zhuzichu.orange.controller
 
+import com.taobao.api.DefaultTaobaoClient
+import com.taobao.api.TaobaoClient
+import com.taobao.api.request.TimeGetRequest
 import com.zhuzichu.orange.core.result.Result
+import com.zhuzichu.orange.core.result.genSuccessResult
 import com.zhuzichu.orange.model.User
 import com.zhuzichu.orange.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
@@ -25,6 +29,14 @@ class UserController {
 
     @PostMapping("/regist")
     fun regist(@Valid user: User, bindingResult: BindingResult): Result {
+//        val client: TaobaoClient = DefaultTaobaoClient(
+//                "http://gw.api.taobao.com/router/rest",
+//                "27560769",
+//                "dfbc7bae26a24e137a73f16298c60a5"
+//        )
+//        val req = TimeGetRequest()
+//        val response = client.execute(req)
+//        if(response.)
         return userService.regist(user)
     }
 }
